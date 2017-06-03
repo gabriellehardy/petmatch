f78a24f1970305968ec4a4f764bbb392abc879ce
gabihardy <gabrhardy@gmail.com>
2017-06-03 18:59:25 -0500

    find.html is nearly complete - ready for api data work to begin

--- assets/javascript/find.js
+++ assets/javascript/find.js
@@ -1,34 +1,47 @@
 $(document).ready(function() {
 
+    
+    $("#Dogs").click(function(){
+            $('.toHide').hide();
+            $("#sizeSelector").show('slow');
+ 
+ }); 
 
 
- var apiKey = "cb6a30e95d827e83a94394acaee07397";
- var url = 'http://api.petfinder.com/pet.find?';
- url += '&format=json&key=' + apiKey;
- console.log(url)
- var petInfo = [];
+    $("#Cats").click(function(){
+            $('.toHide').hide();
+            $("#sizeSelector").hide('slow');
+ 
+ }); 
 
-$.ajax({
-	type: "GET",
-	data: {},
-	url: url + "&callback=?",
-	dataType: "json",
-	success: function(data) {
-		var petfinder = data.petfinder;
-		console.log(petfinder)
-		petInfo = [];
-		for (i=0; i < petfinder.length; i++) {
-			var photo = petfinder[i].name["$t"];
-			var name = petfinder[i].name["$t"];
-			var description = petfinder[i].description["$t"];
-			petInfo.push({description,photo,name});
-	};
-	$("#showPet").attr("src", petInfo[0].photo);
-	$("#name").html(petInfo[0].name);
-	$(".description").html(petInfo[0].description)
-	}
 
-}); //end of ajax call// 
-console.log(petInfo);
+//  var apiKey = "cb6a30e95d827e83a94394acaee07397";
+//  var url = 'http://api.petfinder.com/pet.find?';
+//  url += '&format=json&key=' + apiKey;
+//  console.log(url)
+//  var petInfo = [];
+
+// $.ajax({
+// 	type: "GET",
+// 	data: {},
+// 	url: url + "&callback=?",
+// 	dataType: "json",
+// 	success: function(data) {
+// 		var petfinder = data.petfinder;
+// 		console.log(petfinder)
+// 		petInfo = [];
+// 		for (i=0; i < petfinder.length; i++) {
+// 			var photo = petfinder[i].name["$t"];
+// 			var name = petfinder[i].name["$t"];
+// 			var description = petfinder[i].description["$t"];
+// 			petInfo.push({description,photo,name});
+// 	};
+// 	$("#showPet").attr("src", petInfo[0].photo);
+// 	$("#name").html(petInfo[0].name);
+// 	$(".description").html(petInfo[0].description)
+// 	}
+
+// }); //end of ajax call// 
+// console.log(petInfo);
 
 });      
