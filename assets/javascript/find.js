a895834b301262e7084cf90b16da508133d23c83
Pholtzman88 <holtzman.patrick@gmail.com>
2017-06-05 17:16:59 -0500

    update petfinder search and starting facebook login

--- assets/javascript/find.js
+++ assets/javascript/find.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log('hey i remember you');
 
+        // window.location.href = 'profile.html';
+
+        // User is signed in.
+      } else {
+        window.location.href = 'index.html';
+        console.log('no user');
+        // No user is signed in.
+      }
+    });
     
     $("#dog").click(function(){
             $('.toHide').hide();
@@ -23,43 +35,87 @@ $(document).ready(function() {
 
     $("#dog").on("click", function() {
         console.log("dog selected")
+        $(".editAnimal").removeClass("hidden")
+        $("#animalSelection").addClass("hidden")
+        $("#animalField").prepend("<div id='animalSelected'></div>")
         $("#animalSelected").html("dog")
+
     })
     $("#cat").on("click", function() {
         console.log("cat selected")
+        $(".editAnimal").removeClass("hidden")
+        $("#animalSelection").addClass("hidden")
+        $("#animalField").prepend("<div id='animalSelected'></div>")
         $("#animalSelected").html("cat")
+
     })
     $("#adult").on("click", function() {
         console.log("adult age selected")
+        $(".editAge").removeClass("hidden")
+        $("#ageSelection").addClass("hidden")
+        $("#ageField").prepend("<div id='ageSelected'></div>")
         $("#ageSelected").html("adult")
+
     })
     $("#senior").on("click", function() {
         console.log("senior age selected")
-        $("#ageSelected").html("senior")
+        $(".editAge").removeClass("hidden")
+        $("#ageSelection").addClass("hidden")
+        $("#ageField").prepend("<div id='ageSelected'></div>")
+        $("#ageSelected").html("senior")        
     })
     $("#small").on("click", function() {
         console.log("small size selected")
-        $("#sizeSelected").html("small")
+        $(".editSize").removeClass("hidden")
+        $("#sizeSelection").addClass("hidden")
+        $("#sizeField").prepend("<div id='sizeSelected'></div>")
+        $("#sizeSelected").html("small")        
     })
     $("#medium").on("click", function() {
         console.log("medium size selected")
-        $("#sizeSelected").html("medium")
+        $(".editSize").removeClass("hidden")
+        $("#sizeSelection").addClass("hidden")
+        $("#sizeField").prepend("<div id='sizeSelected'></div>")
+        $("#sizeSelected").html("medium")         
+
     })
     $("#large").on("click", function() {
         console.log("large size selected")
-        $("#sizeSelected").html("large")
+        $(".editSize").removeClass("hidden")
+        $("#sizeSelection").addClass("hidden")
+        $("#sizeField").prepend("<div id='sizeSelected'></div>")
+        $("#sizeSelected").html("large")        
     })
-
+    $(".editAnimal").on("click", function(){
+        console.log("working button")
+        $(".editAnimal").addClass("hidden")
+        $("#animalSelection").removeClass("hidden")
+        $("#animalSelected").remove()       
+    })
+    $(".editAge").on("click", function(){
+        console.log("working button")
+        $(".editAge").addClass("hidden")
+        $("#ageSelection").removeClass("hidden")
+        $("#ageSelected").remove()       
+    })
+    $(".editSize").on("click", function(){
+        console.log("working button")
+        $(".editSize").addClass("hidden")
+        $("#sizeSelection").removeClass("hidden")
+        $("#sizeSelected").remove()       
+    })
     $("#save").on("click", function(e) {
         e.preventDefault();
-        //$("#searchPets").modal("toggle")
+        //clears petInfo 
+        petInfo = [];
+
         function ajax(){
             zipcode = $("#zipcode").val().trim();
             animal = $("#animalSelected").text();
             age = $("#ageSelected").text();
             size = $("#sizeSelected").text();
             var apiKey = "cb6a30e95d827e83a94394acaee07397";
-            var url = 'http://api.petfinder.com/pet.find?';
+            var url = 'https://api.petfinder.com/pet.find?';
             if (zipcode.length = 5){
                 url += '&location=' + zipcode;
             }
@@ -86,21 +142,22 @@ $(document).ready(function() {
                     var petfinder = data.petfinder.pets.pet;
                     console.log(petfinder)
                     //gets all animal info and pushes to petInfo array
-                    //petInfo = [];
                     for (i=0;i<petfinder.length;i++){
                         var photo = petfinder[i].media.photos.photo[2]["$t"];
                         var name = petfinder[i].name["$t"];
                         var description = petfinder[i].description["$t"];
-                        //var petId = petfinder.id["$t"];
+                        var pid = petfinder[i].id["$t"];
+                        var email = petfinder[i].contact.email["$t"]
 
                         if (description == null){
                             description = "No description available"
                         }
                         else if (photo == null){
                             /*photo = url(insert photo location here)*/
+
                         }
                  
-                        petInfo.push({description,photo,name});
+                        petInfo.push({description,photo,name,pid,email});
                     }; 
                     //sets first animal info
                     $(".logged-in-img").attr("id","newImg");
